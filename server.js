3280f95f1ddd70806c9d47d50681c63de27f3746
idlviv <idlviv@rambler.ru>
2017-03-22 15:53:55 +0200

    just started

--- server.js
+++ server.js
@@ -17,9 +17,46 @@ var urlUtils = require('url');
 
 app.get('/', function(req, res) {
   res.render('template', {
-    title: 'Введіть слово',
+    title: 'Введіть слово'
   });
+});
 
+app.post('/', function(req, res){
+  if (!req.body.text || req.body.text == "") {
+    res.render('template', {
+      title: "Введіть слово"
+    });
+  } else {
+    var url = urlUtils.format({
+      protocol: 'https',
+      hostname: 'translate.yandex.net',
+      pathname: 'api/v1.5/tr.json/translate',
+      query: {
+        key: 'trnsl.1.1.20140416T130443Z.49db75a946e5d9df.baa803157e4482838c0612cb9c5aa513643049a4',
+        lang: req.body.lang,
+        text: req.body.text
+      }
+    });
+    requestUtils.get({
+        url: url,
+        json: true
+      }, function (error, response, json) {
+        var data = {};
+
+        if (error || json.code != 200) {
+          data = {
+            title: "Ошибка при переводе слова " + req.body.text,
+            error: json.message
+          }
+        } else {
+          data = {
+            title: 'Перевод слова ' + req.body.text + ": " + json.text
+          }
+        }
+        res.render('template', data);
+      }
+    );
+  }
 });
 
 app.listen(8080, function(err) {
