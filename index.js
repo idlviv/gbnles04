66dfd358494233beccbcbf85fa356e70d2f66569
idlviv <idlviv@rambler.ru>
2017-03-23 15:13:49 +0200

    just started

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var inp = document.body.firstElementChild.children[1];
 console.log(inp);
 var elems = inp.getElementsByTagName('input');
-console.log(elems);
+elems[1].setAttribute('id','text');
+console.dir(elems[1].value = 'a33');
+console.dir(elems[1].className);
